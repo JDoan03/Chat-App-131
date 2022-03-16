b875a28ca1b8efd9c4f3e896e8b87916d163a784
JDoan03 <josephdoan03@gmail.com>
2022-03-15 19:00:41 -0700

    Jackson

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './cutie.PNG';
 import './App.css';
 
 function App() {
@@ -7,19 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Hola! Soy Dora.
         </p>
-        <p>
-            lorem
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
