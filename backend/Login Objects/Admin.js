1fa096e1cef927a61d2d0abbc75b2db58262f27e
Niketa Kosyuk <nkosyuk@csus.edu>
2022-03-30 18:24:56 -0700

    Java Doc Addition
    Documentation added to Jason's commit

--- backend/Login Objects/Admin.js
+++ backend/Login Objects/Admin.js
@@ -1,9 +1,26 @@
+/**
+ * The Admin class is a subclass of the User class. 
+ * The Admin object will inherit all of the User objects 
+ * methods, while also having unique capablities on creating
+ * student and teacher objects. Lastly, the Admin object will have
+ * the ability to reset passwords. 
+ * @author Jason Yu
+ */
 class Admin extends User{
     #first;
     #last;
     #email;
     #phone;
 
+    /**
+     * Regular Constructor for the admin object.  
+     * @param {*} username 
+     * @param {*} password 
+     * @param {*} first 
+     * @param {*} last 
+     * @param {*} email 
+     * @param {*} phone 
+     */
     constructor(username, password, first, last, email, phone){
         super(username, password);
         this.#first = first;
@@ -12,14 +29,36 @@ class Admin extends User{
         this.#phone = phone;
     }
 
+    /**
+     * The CreateTeacher method will allow the Admin user to create teacher 
+     * users. 
+     * @param {*} username 
+     * @param {*} password 
+     * @param {*} first 
+     * @param {*} last 
+     * @param {*} email 
+     * @param {*} phone 
+     */
     CreateTeacher(username, password, first, last, email, phone){
         const teacher = new Teacher(username, password, first, last, email, phone)
     }
 
+    /**
+     * The CreateStudent method will allow the Admin user to create student 
+     * users.
+     * @param {*} user 
+     * @param {*} pass 
+     * @param {*} first 
+     * @param {*} last 
+     */
     CreateStudent(user, pass, first, last){
         const student = new Student(user, pass, first, last);
     }
     
+    /**
+     * The Admin object has the ability to reset their own password. 
+     * @param {*} newPass 
+     */
     ResetPassword(newPass){
         super.setPassword = newPass;
     }
--- backend/Login Objects/Student.js
+++ backend/Login Objects/Student.js
@@ -1,7 +1,20 @@
+/**
+ * The student class is a sub class of the User class.
+ * The Student object does not have any unqiue methods 
+ * and will be overseen by a Teacher user.
+ * @author Jason Yu
+ */
 class Student extends User{
     #first;
     #last;
     
+    /**
+     * Regular Constructor for the Student object.
+     * @param {*} username 
+     * @param {*} password 
+     * @param {*} first 
+     * @param {*} last 
+     */
     constructor(username, password, first, last){
         super(username, password);
         this.#first = first;
